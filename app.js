3e2049d6bd5c61cab69ffb74acde4f84ea768cb1
Amirkhon-Buriev <amirkhonburiyev@gmail.com>
2024-09-14 10:37:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let body = document.querySelector('body');
+
+body.style.display = 'flex'
+body.style.height = '100vh'
+body.style.justifyContent = 'center'
+body.style.alignItems = 'center'
+body.style.backgroundColor= prompt('rang kirit ingiliz tilidg')
+
+let ism = document.createElement('h1')
+ism.innerHTML = ('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+ism.style.backgroundColor= prompt('orqa rang kirit ingiliz tilidg')
+ism.style.color= prompt('text rang kirit ingiliz tilidg')
+body.appendChild(ism)
